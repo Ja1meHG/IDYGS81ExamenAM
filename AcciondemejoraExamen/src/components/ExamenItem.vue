052d0fa9d256d9df11cc25933986dd5a01aa4de9
Ja1meHG <106701147+Ja1meHG@users.noreply.github.com>
2024-03-09 13:48:36 -0500

    Add files via upload

---
+++ AcciondemejoraExamen/src/components/ExamenItem.vue
@@ -0,0 +1,70 @@
+<template>
+    <h2>{{ examen.title }}</h2>
+    <select @change="consola(selectedOption)" v-model="selectedOption">
+        <option v-for="(option,index) in examen.options" :key="index" :value="index + 1">{{ option }}</option>
+    </select>
+    <p>Respuesta  {{ selectedOption }}</p>
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue';
+import type { IExamen } from '@/interfaces/IExamen';
+const { examen } = defineProps(['examen']);
+const selectedOption = ref<number>(examen.answer);
+
+const consola = (index: number) => {
+    console.log(`Pregunta ID: ${examen.id}, Opción seleccionada: ${index}`);
+};
+
+</script>
+<style scoped>
+    .formulario{
+        width: 500px;
+        height: 360px;
+    }
+    .errorAp, .ApLargo, .errorte, .NLargo, .tel{
+        color: red;
+    }
+    input{
+        width: 60%;
+        height: 28px;
+        border-radius: 50px;
+        border: none;
+        background-color: cadetblue;
+    }
+.otro{
+    margin-top: 10px;
+    width: 60%;
+}
+    span{
+        width: 100%;
+        display: block;
+    }
+    select{
+        width: 60%;
+        height: 25px;
+        border-radius: 100px;
+        border: none;
+        background-color: cadetblue;
+    }
+
+    option{
+        font-size: 15px;
+        text-align: center;
+    }
+    h3{
+        font-size: 13px;
+    }
+    button{
+        display: flex;
+        width: 100px;
+        border-radius: 20px;
+        border: none;
+        margin-top: 20px;
+        margin-left: 40%;
+        margin-bottom: 40px;
+}
+button:hover{
+    cursor: pointer;
+    background-color: cornflowerblue;
+}
+</style>
---
+++ AcciondemejoraExamen/src/components/ExamenList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <ExamenItem v-for="examen in quizData" :key="examen.id" :examen="examen" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import ExamenItem from '@/components/ExamenItem.vue';
+import QuizData from '@/data/ExamenData';
+
+const quizData = ref(QuizData);
+
+
+</script>
+<style scoped></style>
